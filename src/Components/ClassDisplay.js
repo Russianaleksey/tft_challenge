30cae70a25604c7b509842445929beeab3390d38
Russianaleksey <alexmamontovprofessional@gmail.com>
2019-07-07 22:17:51 -0400

    other fixes

--- src/Components/ClassDisplay.js
+++ src/Components/ClassDisplay.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 import React from 'react'
 import ChampDisplay from './ChampDisplay'
 const ClassDisplay = ({ classes }) => {
@@ -18,25 +18,4 @@ const ClassDisplay = ({ classes }) => {
     )
 }
 
-=======
-import React from 'react'
-import ChampDisplay from './ChampDisplay'
-const ClassDisplay = ({ classes }) => {
-    
-
-    return (
-        <div>
-            {classes.map(({ name, champions }) => {
-                return(
-                    <h2>
-                        {name}
-                        {champions.map(({ champ_name, img }) => <ChampDisplay champ_name={champ_name} img={img} />)}
-                    </h2>
-                )
-            })}
-        </div>
-    )
-}
-
->>>>>>> 564294359966e2e6f56c21b41de352c7ec18416c
 export default ClassDisplay
