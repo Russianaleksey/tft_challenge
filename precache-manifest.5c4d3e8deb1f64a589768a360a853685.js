4d03acb55f5cde9220abeb59c31b51c56dabdb48
Russianaleksey <alexmamontovprofessional@gmail.com>
2019-07-07 22:18:22 -0400

    Updates

---
+++ precache-manifest.5c4d3e8deb1f64a589768a360a853685.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30d9b960ae03b461592513f9416fc4bb",
+    "url": "/tft_challenge/index.html"
+  },
+  {
+    "revision": "6164e1dfce114fddf222",
+    "url": "/tft_challenge/static/css/main.797c576c.chunk.css"
+  },
+  {
+    "revision": "407ab141deba78abe319",
+    "url": "/tft_challenge/static/js/2.d042e1ad.chunk.js"
+  },
+  {
+    "revision": "6164e1dfce114fddf222",
+    "url": "/tft_challenge/static/js/main.28595eea.chunk.js"
+  },
+  {
+    "revision": "12f640328a49b95c0b22",
+    "url": "/tft_challenge/static/js/runtime~main.2ee6b358.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/tft_challenge/static/media/logo.5d5d9eef.svg"
+  }
+]);
