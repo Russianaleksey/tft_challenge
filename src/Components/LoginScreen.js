135a4e2e39664e9daa893b8ab7ed8aa21d1b179c
Russianaleksey <alexmamontovprofessional@gmail.com>
2019-07-20 23:52:04 -0400

    added styling

--- src/Components/LoginScreen.js
+++ src/Components/LoginScreen.js
@@ -5,7 +5,10 @@ const LoginScreen = () => {
         <div className="login-screen">
             <h1 className="login-screen__header">Summoner Login</h1>
             <form>
-                <input className="login-screen__username-field" type="text" placeholder="Username"></input>
+                <input className="login-screen__username-field" type="text" placeholder="Username" />
+            </form>
+            <form>
+                <input className="login-screen__password-field" type="password" placeholder="Password" />
             </form>
         </div>
     )
