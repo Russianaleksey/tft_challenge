87afe1eb2e6527badd37ac0f8fa38a78416ee470
Jordan Dobson <jordantdobson@gmail.com>
2019-07-28 13:06:03 -0400

    flipping done

--- src/Components/ChampDisplay.js
+++ src/Components/ChampDisplay.js
@@ -4,17 +4,15 @@ import React from 'react'
 const ChampDisplay = ({ champ_name, img, origin, champ_class, tier }) => {
     return (
         <div className={"champ-display"}>
-            <div id="carddiv" className="card" onClick={(id) => classToggle(id)}>
-                <div className="champ-card-back champ-card">Back</div>
-                <div className="champ-card-image champ-card">Front</div>
+            <div id={champ_name} className="card" onClick={() => classToggle(champ_name)}>
+                <img className="champ-card-back champ-card" src={"https://i.pinimg.com/originals/fd/0c/b9/fd0cb97ac9aaa1341195b1c4ab58fb6f.png"}/>
+                <img className="champ-card-image champ-card" src={img}/>
             </div>
         </div>
     )
 }
 
-
 function classToggle(element) {
-    console.log(document.getElementById(element));
     if(document.getElementById(element).className == "card") {
         document.getElementById(element).className = "card is-flipped";
     }
